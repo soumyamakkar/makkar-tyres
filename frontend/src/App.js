cd0a7e03eafaef6f9baf13dc9658be89ae55819b
SoumyaMakkar <soumyamakkar123@gmail.com>
2024-10-26 23:04:32 +0530

    trying...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import GenerateEmail from './components/GenerateEmail';
+import NotFound from './components/NotFound'; // Import the NotFound component
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/generate-email" element={<GenerateEmail />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/generate-email" element={<GenerateEmail />} />
+                
+                {/* Fallback route for 404 */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
