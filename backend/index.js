1e4d2e0325194d89f60307a2ced26ba558081917
SoumyaMakkar <soumyamakkar123@gmail.com>
2024-10-27 11:55:57 +0530

    making changes

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,7 @@ require('dotenv').config();
 const emailRouter=require('./routes/email');
 const app=express();
 const PORT=process.env.PORT||9002;
-app.use(cors({
-    origin: '*', // Or specify your domain like 'https://your-domain.com'
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use("/",emailRouter);
